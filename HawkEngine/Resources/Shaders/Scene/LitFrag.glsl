319589825d5e516bbcfcdd0f5685686b58bf66e3
LucoseGlucose <86197206+LucoseGlucose@users.noreply.github.com>
2023-03-02 15:46:17 -0500

    Idk

--- HawkEngine/Resources/Shaders/Scene/LitFrag.glsl
+++ HawkEngine/Resources/Shaders/Scene/LitFrag.glsl
@@ -121,7 +121,7 @@ vec3 fresnelSchlick(float cosTheta, vec3 F0)
 vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness)
 {
     return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
-}   
+}
 
 float random(vec3 seed, int i)
 {
